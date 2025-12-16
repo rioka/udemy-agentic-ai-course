ff09bee068612340bb800d469b6b7688105ffa0a
rioka <rioka@libero.it>
2025-12-16 15:49:34 +0000

    Load more keys

--- 01-foundations/main.py
+++ 01-foundations/main.py
@@ -6,13 +6,24 @@ def main() -> None:
     loaded = dotenv.load_dotenv(override=True)
     
     if loaded:
-        openai_api_key = os.getenv('OPENAI_API_KEY')
-        if openai_api_key:
-            print(f"OpenAI API Key exists and begins with {openai_api_key[:8]}")
-        else:
-            print("OpenAI API Key not set")
+        openai_api_key = try_get_key('OPENAI_API_KEY')
+        anthropic_api_key = try_get_key('ANTHROPIC_API_KEY')
+        google_api_key = try_get_key('GOOGLE_API_KEY')
+        deepseek_api_key = try_get_key('DEEPSEEK_API_KEY')
+        groq_api_key = try_get_key('GROQ_API_KEY')
     else:
         print("Unable to load .env file")
+
+def try_get_key(key: str) -> str | None:
+     
+     value = os.getenv(key)
+     
+     if value:
+            print(f"Key {key} exists and begins with {value[:8]}")
+     else:
+            print(f"Key {key} not set")
+     
+     return value
 
 if __name__ == "__main__":
     main()
