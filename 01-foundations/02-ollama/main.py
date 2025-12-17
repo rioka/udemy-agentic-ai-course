1f475bbb7702a487e57d1e3023fb1730ee52c7ed
rioka <rioka@libero.it>
2025-12-17 12:24:53 +0000

    Add evaluation step

--- 01-foundations/02-ollama/main.py
+++ 01-foundations/02-ollama/main.py
@@ -1,8 +1,7 @@
 import openai
 import os
 import openai.types.chat
-#import IPython.display
-from IPython.display import Markdown, display
+# from IPython.display import Markdown, display
 
 def main() -> None:
 
@@ -11,18 +10,30 @@ def main() -> None:
 
     question = get_question(f'{ollama_host}/v1', model_name)
 
-    print(question)
+    print(f"**Question**:\n{question}\n")
 
     if question is None:
         print("No question to ask")
         return
 
     answer = send_question(f'{ollama_host}/v1', model_name, question)
-    print(answer)
-    ##IPython.display.display(IPython.display.Markdown(answer))
-    #display(Markdown(answer))
+    # Using plain `print` here, because we're running it from the console
+    print(f"**Answer**:\n{answer}\n")
+    # display(Markdown(answer))
     #competitors.append(model_name)
     #answers.append(answer)
+
+    if answer is None:
+        print("No answer was provided")
+        return
+
+    evaluation = evaluate_response(f'{ollama_host}/v1', 'llama3.2', question, answer)
+
+    if evaluation is None:
+        print("No evaluation was provided")
+        return
+
+    print(f"**Evaluation**:\n{evaluation}")
 
 def get_question(host: str, model: str) -> str | None:
 
@@ -41,6 +52,36 @@ def send_question(host: str, model: str, question: str) -> str | None:
     client = openai.OpenAI(base_url=host, api_key='ollama')
     response = client.chat.completions.create(model=model, messages=messages)
     return response.choices[0].message.content
+
+def evaluate_response(host: str, model: str, question: str, answer: str) -> str | None:
+
+    judge = f"""You are judging an answer tothis question:
+
+    {question}
+
+    Your job is to evaluate the response for clarity and strength of argument, and rank it.
+    Respond with JSON, and only JSON, with the following format:
+    {{"grade": "...", "evaluation": "..."}}
+    where 
+    - "grade" is a letter A to F, A being the best and F the worst
+    - "evaluation" is your explanation for the grade you've assigned
+    
+    Here is the response you must evaluate:
+
+    {answer}
+
+    Now respond with the JSON I've given, nothing else."""
+
+    messages: list[openai.types.chat.ChatCompletionMessageParam]  = [{"role": "user", "content": judge}]
+
+    client = openai.OpenAI(base_url=host, api_key='ollama')
+    response = client.chat.completions.create(
+        model=model, 
+        messages=messages
+    )
+
+    return response.choices[0].message.content
+
 
 if __name__ == "__main__":
     main()
