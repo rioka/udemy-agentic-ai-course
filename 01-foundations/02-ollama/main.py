df8d5328a384b4a8de20e43dfbb1a523e2434033
rioka <rioka@libero.it>
2025-12-17 14:01:53 +0000

    Send question to both LLMs

--- 01-foundations/02-ollama/main.py
+++ 01-foundations/02-ollama/main.py
@@ -5,29 +5,42 @@ import openai.types.chat
 
 def main() -> None:
 
-    ollama_host = os.getenv('OLLAMA_HOST')
-    model_name = os.getenv('OLLAMA_MODEL', 'llama3.2:1b')
+    ollama_host = os.getenv('OLLAMA_HOST', 'http://localhost:11434')
+    ollama_model = os.getenv('OLLAMA_MODEL', 'llama3.2:1b')
 
-    question = get_question(f'{ollama_host}/v1', model_name)
+    localai_host = os.getenv('LOCALAI_HOST', 'http://localhost:8765')
+    localai_model = os.getenv('LOCALAI_MODEL', 'dolphin3.0-qwen2.5-3b')
+
+    question = get_question(f'{ollama_host}/v1', ollama_model)
 
     print(f"**Question**:\n{question}\n")
 
     if question is None:
         print("No question to ask")
         return
+    
+    answers = []
 
-    answer = send_question(f'{ollama_host}/v1', model_name, question)
+    print("**Asking Ollama**\n")
+    answer = send_question(f'{ollama_host}/v1', ollama_model, question)
     # Using plain `print` here, because we're running it from the console
-    print(f"**Answer**:\n{answer}\n")
-    # display(Markdown(answer))
-    #competitors.append(model_name)
-    #answers.append(answer)
-
     if answer is None:
         print("No answer was provided")
         return
 
-    evaluation = evaluate_response(f'{ollama_host}/v1', 'llama3.2', question, answer)
+    print(f"**Answer (Ollama)**:\n{answer}\n")
+    answers.append(answer)
+
+    print("**Asking LocalAI**\n")
+    answer = send_question(f'{localai_host}/v1', localai_model, question)
+    if answer is None:
+        print("No answer was provided")
+        return
+
+    print(f"**Answer (LocalAI)**:\n{answer}\n")
+    answers.append(answer)
+
+    evaluation = evaluate_response(f'{ollama_host}/v1', 'llama3.2', question, answers)
 
     if evaluation is None:
         print("No evaluation was provided")
@@ -53,7 +66,7 @@ def send_question(host: str, model: str, question: str) -> str | None:
     response = client.chat.completions.create(model=model, messages=messages)
     return response.choices[0].message.content
 
-def evaluate_response(host: str, model: str, question: str, answer: str) -> str | None:
+def evaluate_response(host: str, model: str, question: str, answers: list[str]) -> str | None:
 
     judge = f"""You are judging an answer tothis question:
 
@@ -61,14 +74,17 @@ def evaluate_response(host: str, model: str, question: str, answer: str) -> str 
 
     Your job is to evaluate the response for clarity and strength of argument, and rank it.
     Respond with JSON, and only JSON, with the following format:
-    {{"grade": "...", "evaluation": "..."}}
+    {[{"grade": "...", "evaluation": "..."}]}
     where 
     - "grade" is a letter A to F, A being the best and F the worst
     - "evaluation" is your explanation for the grade you've assigned
     
-    Here is the response you must evaluate:
+    The order of your response must match the order of the responses, so the first element is for the first answer, 
+    the second for the second answer, and so on.
 
-    {answer}
+    Here is the responses you must evaluate:
+
+    {answers}
 
     Now respond with the JSON I've given, nothing else."""
 
