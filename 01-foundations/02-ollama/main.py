875034f4e90d095b0b836e270fd9bdb75eb825e3
rioka <rioka@libero.it>
2025-12-17 10:30:57 +0000

    WIP

--- 01-foundations/02-ollama/main.py
+++ 01-foundations/02-ollama/main.py
@@ -1,20 +1,33 @@
 import openai
 import os
-from openai.types.chat import ChatCompletionMessageParam  # <-- typed dict
+import openai.types.chat
+#import IPython.display
+from IPython.display import Markdown, display
 
 def main() -> None:
 
-    request = "Please come up with a challenging, nuanced question that I can ask a number of LLMs to evaluate their intelligence. "
-    request += "Answer only with the question, no explanation."
-    messages: list[ChatCompletionMessageParam] = [{"role": "user", "content": request}]    
-
-    model_name = "llama3.2" # 3B / 2GB
     ollama_host = os.getenv('OLLAMA_HOST')
-    ollama = openai.OpenAI(base_url=f'{ollama_host}/v1', api_key='ollama')
+    model_name = os.getenv('OLLAMA_MODEL', 'llama3.2:1b')
 
-    response = ollama.chat.completions.create(model=model_name, messages=messages)
-    answer = response.choices[0].message.content
+    question = get_question(f'{ollama_host}/v1', model_name)
 
+    print(question)
+
+    ##IPython.display.display(IPython.display.Markdown(answer))
     #display(Markdown(answer))
     #competitors.append(model_name)
-    #answers.append(answer)    
+    #answers.append(answer)
+
+def get_question(host: str, model: str) -> str | None:
+
+    request = "Please come up with a challenging, nuanced question that I can ask a number of LLMs to evaluate their intelligence. "
+    request += "Answer only with the question, no explanation."
+    messages: list[openai.types.chat.ChatCompletionMessageParam] = [{"role": "user", "content": request}]
+
+    ollama = openai.OpenAI(base_url=host, api_key='ollama')
+    response = ollama.chat.completions.create(model=model, messages=messages)
+
+    return response.choices[0].message.content
+
+if __name__ == "__main__":
+    main()
